6f5a0a4e5d72369f576668128bf2191f1d85a64b
Marlon4789 <cuartasm844@gmail.com>
2024-03-30 18:56:03 -0500

    creando app portafolio, biografia

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from portafolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_coffee_store.urls'))
+    path('', views.home, name='home'),
+    path('api_coffee_store/', include('api_coffee_store.urls'))
 ]
