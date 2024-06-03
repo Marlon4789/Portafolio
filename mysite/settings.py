2dc8b30ce1f87425b3833a3dd5a7ad1cb944d5d6
Marlon4789 <cuartasm844@gmail.com>
2024-06-03 18:10:00 -0500

    .gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4*po-e=xk=rf%y+4q#6_kj5l5)&egnxhtw(z3-!_fwc0j&fmnu'
+#SECRET_KEY = 'django-insecure-4*po-e=xk=rf%y+4q#6_kj5l5)&egnxhtw(z3-!_fwc0j&fmnu'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
